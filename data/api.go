c5ed7ed3e56eaee727dfb3204f893d4bc017aa8d
hayabusa2yk <hayabusa2.yk@gmail.com>
2022-09-03 21:34:01 +0900

    Make first letter of json fields lower case

--- data/api.go
+++ data/api.go
@@ -1,43 +1,43 @@
 package data
 
 type Health struct {
-	Health  string
-	Message string
+	Health  string `json:"health" xml:"health" form:"health"`
+	Message string `json:"message" xml:"message" form:"message"`
 }
 
 type Error struct {
-	Code   string
-	Reason string
+	Code   string `json:"code" xml:"code" form:"code"`
+	Reason string `json:"reason" xml:"reason" form:"reason"`
 }
 
 type RoomCreateRequest struct {
-	Name string
+	Name string `json:"name" xml:"name" form:"name"`
 }
 
 type RoomCreate struct {
-	Id int64
+	Id int64 `json:"id" xml:"id" form:"id"`
 }
 
 type RoomConnectRequest struct {
-	Name string
+	Name string `json:"name" xml:"name" form:"name"`
 }
 
 type Room struct {
-	Id      int64
-	Name    string
-	Devices []Device
+	Id      int64    `json:"id" xml:"id" form:"id"`
+	Name    string   `json:"name" xml:"name" form:"name"`
+	Devices []Device `json:"devices" xml:"devices" form:"devices"`
 }
 
 type Device struct {
-	Name      string
-	Timestamp int64
+	Name      string `json:"name" xml:"name" form:"name"`
+	Timestamp int64  `json:"timestamp" xml:"timestamp" form:"timestamp"`
 }
 
 type Status struct {
-	Device    string
-	Timestamp int64
-	Type      string
-	Name      string
-	Mime      string
-	Hash      string
+	Device    string `json:"device" xml:"device" form:"device"`
+	Timestamp int64  `json:"timestamp" xml:"timestamp" form:"timestamp"`
+	Type      string `json:"type" xml:"type" form:"type"`
+	Name      string `json:"name" xml:"name" form:"name"`
+	Mime      string `json:"mime" xml:"mime" form:"mime"`
+	Hash      string `json:"hash" xml:"hash" form:"hash"`
 }
